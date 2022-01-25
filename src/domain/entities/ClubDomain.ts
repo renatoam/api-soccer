b4a03a6f2d24a74ae6849503d5dd737902264ed3
˜Renato˜ <renato.melo@ab-inbev.com>
2022-01-25 20:49:53 -0300

    feat: create club domain

---
+++ src/domain/entities/ClubDomain.ts
@@ -0,0 +1,29 @@
+import { Club } from "../../types";
+
+export class ClubDomain implements Club {
+  public readonly id: string;
+  public readonly name: string;
+  public readonly nickname?: string | undefined;
+  public readonly site?: string | undefined;
+  public readonly foundation: string;
+  public readonly country: string;
+  public readonly locale?: string | undefined;
+  public readonly mascot?: string | undefined;
+  public readonly history: string;
+  
+  constructor(club: Club) {
+    this.id = club.id
+    this.name = club.name
+    this.nickname = club.nickname
+    this.site = club.site
+    this.foundation = club.foundation
+    this.country = club.country
+    this.locale = club.locale
+    this.mascot = club.mascot
+    this.history = club.history
+  }
+
+  async validateName() {
+    return this.name && typeof this.name === 'string'
+  }
+}
