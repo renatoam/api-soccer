1ede6f35f223f1c866cd1032b4983f2c547fbaf2
˜Renato˜ <renato.melo@ab-inbev.com>
2022-01-25 20:55:30 -0300

    test: create tests for club domain

---
+++ tests/domain/entities/ClubDomain.test.ts
@@ -0,0 +1,23 @@
+import { ClubDomain } from "../../../src/domain/entities/ClubDomain"
+
+const clubMock: any = {
+  id: "1",
+  name: 'Renato',
+  foundation: "1920",
+  country: "Brazil",
+  history: "Blablablabla"
+}
+
+describe('Club domain', () => {
+  it('should name be truthy', () => {
+    const club = new ClubDomain(clubMock)
+
+    expect(!!club.name).toBe(true)
+  })
+  
+  it('should name type be string', () => {
+    const club = new ClubDomain(clubMock)
+
+    expect(typeof club.name).toBe('string')
+  })
+})
