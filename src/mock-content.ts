adc16669871bb030ebac10944f75986cd162b7ec
˜Renato˜ <renato.melo@ab-inbev.com>
2022-02-10 19:15:25 -0300

    refactor: change mock content object

--- src/mock-content.ts
+++ src/mock-content.ts
@@ -1,76 +1,89 @@
 export const clubMock = {
-  club: {
-      name: "São Paulo Futebol Clube",
-      nickname: "O mais querido",
-      site: "https://spfc.net/",
-      foundation: "25 de janeiro de 1930",
-      stadium: "Morumbi",
-      country: "Brasil",
-      state: "São Paulo",
-      capacity: "72.039",
-      mascot: "Santo Paulo",
-      rivals: ['Corinthians', 'Palmeiras', 'Santos'],
-      history: "São Paulo, ou SPFC, é um time de futebol do Brasil. Foi fundado em 25 de janeiro de 1930 e sua sede é na cidade de São Paulo, mais precisamente no bairro do Morumbi.<br/> Manda seus jogos no estádio do Morumbi Cícero Pompeu de Toledo, que tem capacidade para mais de setenta mil torcedores, e é o maior estádio particular do futebol brasileiro.<br/> É o clube brasileiro com mais conquista no Mundial Interclubes (1992, 1993 e 2005), é um dos clubes que possuem mais libertadores da américa no Brasil, vencendo o torneio por 3x (1992, 1993 e 2005), além das conquistas da sul-americana e recopa.<br/> Têm vinte e dois campeonatos paulistas e já foi campeão do campeonato brasileiro por 06 vezes (1977, 1986, 1991, 2006,2007 e 08).<br/> Seus maiores rivais no futebol brasileiro são o Sport Clube Corinthians Paulista, Sociedade Esportiva Palmeiras e o Santos Futebol Clube."
+  profile: {
+    name: "São Paulo Futebol Clube",
+    nickname: "O mais querido",
+    site: "https://spfc.net/",
+    foundation: "25 de janeiro de 1930",
+    stadium: "Morumbi",
+    country: "Brasil",
+    state: "São Paulo",
+    capacity: "72.039",
+    mascot: "Santo Paulo",
+    rivals: ['Corinthians', 'Palmeiras', 'Santos'],
+    history: "São Paulo, ou SPFC, é um time de futebol do Brasil. Foi fundado em 25 de janeiro de 1930 e sua sede é na cidade de São Paulo, mais precisamente no bairro do Morumbi.<br/> Manda seus jogos no estádio do Morumbi Cícero Pompeu de Toledo, que tem capacidade para mais de setenta mil torcedores, e é o maior estádio particular do futebol brasileiro.<br/> É o clube brasileiro com mais conquista no Mundial Interclubes (1992, 1993 e 2005), é um dos clubes que possuem mais libertadores da américa no Brasil, vencendo o torneio por 3x (1992, 1993 e 2005), além das conquistas da sul-americana e recopa.<br/> Têm vinte e dois campeonatos paulistas e já foi campeão do campeonato brasileiro por 06 vezes (1977, 1986, 1991, 2006,2007 e 08).<br/> Seus maiores rivais no futebol brasileiro são o Sport Clube Corinthians Paulista, Sociedade Esportiva Palmeiras e o Santos Futebol Clube."
   },
-  legends: [{
+  legends: [
+    {
       name: "Rogerio Ceni",
       position: "Goleiro",
       avatar: "http://www.saopaulofc.net/media/176764/rc-idolos.jpg",
       description: "Maior goleiro artilheiro do mundo, o M1TO são-paulino deixou os gramados em 11 de dezembro de 2015, em uma noite memorável, mas está eternizado. Para muitos o maior ídolo são-paulino de todos os tempos, o colecionador de recordes permeia mais de um quarto da história do São Paulo Futebol Clube. Capitão de tantas glórias, levantou nesse período mais taças do que clubes em toda uma existência. Goleiro. Artilheiro. M1TO. São-Paulino."
-  }, {
+    },
+    {
       name: "Cicinho",
       position: "Lateral-direito",
       avatar: "http://www.saopaulofc.net/media/17292/cicinho2.png",
       description: "Lateral extremamente ofensivo, um verdadeiro ala, Cicinho fez parte do esquadrão são-paulino de 2005, Campeão da Libertadores e do Mundo.Além do apoio ao ataque, outra característica marcante do jogador eram os chutes potentes de fora da área."
-  }, {
+    },
+    {
       name: "Diego Lugano",
       position: "Zagueiro",
       avatar: "http://www.saopaulofc.net/media/2427/Lugano.jpg",
       description: "Desconhecido, sua contratação não fora aprovada pelo então técnico, Oswaldo de Oliveira, passando a ser conhecido como 'O Jogador do Presidente'. Aos poucos, jogando com uma grande vontade e espírito de luta Lugano conquistou a torcida são-paulina e o apelido de 'DIO5'."
-  }, {
+    },
+    {
       name: "Miranda",
       position: "Zagueiro",
       avatar: "http://www.saopaulofc.net/media/27937/spmiranda_wr_281011_crop_galeria.jpg",
       description: "Chegava com a difícil missão de substituir o ídolo uruguaio Diego Lugano e a obrigação de assumir a camisa 5 de um dos maiores ídolos do clube. Sério, tranquilo e eficiente, o jovem zagueiro não custou a conquistar seu espaço dentro de campo, sagrando-se tricampeão brasileiro (06, 07 e 08)"
-  }, {
+    },
+    {
       name: "Leonardo",
       position: "Lateral-esquerdo",
       avatar: "http://www.saopaulofc.net/media/17282/leonardo.png",
       description: "Um craque cuja cotação alcançou níveis comparáveis a Zico, Bebeto e outros grandes nomes do futebol brasileiro. Campeão Mundial em 1993, no ano seguinte também conquistou o mundo pela Seleção Brasileira. Durante sua carreira jogou tanto como lateral-esquerdo como meia-esquerda."
-  }, {
+    },
+    {
       name: "Mineiro",
       position: "Volante",
       avatar: "http://www.saopaulofc.net/media/17281/mineiro3.png",
       description: "Gaúcho de Porto Alegre, Mineiro se consagrou no Tricolor pelo gol sobre o Liverpool, na final do Mundial de Clubes de 2005. Seu futebol, fôlego e vigor, porém, já bastariam para enquadrá-lo entre os melhores de todos os tempos. Exímio marcador, do gênero incansável, ainda arriscava avanços ao ataque e chutes de longas distâncias."
-  }, {
+    },
+    {
       name: "Raí",
       position: "Meia",
       avatar: "http://www.saopaulofc.net/media/2277/rai.jpg",
       description: "Veio do Botafogo de Ribeirão Preto em 87 e demorou pouco para brilhar. Tinha o estigma de jogador lento. Tornou-se o capitão e virou uma espécie de símbolo do time que ganhou quase tudo em 91, 92 e 93. Raí jogou na França e voltou ao tricolor em 1998 para encerrar sua carreira em 2000."
-  }, {
+    },
+    {
       name: "Kaká",
       position: "Meia",
       avatar: "http://www.saopaulofc.net/media/17279/kaka1.png",
       description: "Autêntica jóia tricolor, Kaká foi lapidado desde criança para ser uma verdadeira potência entre os atletas. Bom caráter, inteligente, forte, ágil, veloz... Poucas são as características positivas a um jogador que ele não possui. Em suas primeiras partidas no Tricolor, de cara foi campeão, marcando dois gols na final da competição. Foi Campeão do Mundo em 2002 pela Seleção e escolhido o Melhor do Mundo pela FIFA, em 2007."
-  }, {
+    },
+    {
       name: "França",
       position: "Centroavante",
       avatar: "http://www.saopaulofc.net/media/17277/franca1.png",
       description: "O centroavante foi um grande ídolo são-paulino de um período de vacas magras do clube. Ainda assim, França entrou para a galeria de grandes jogadores do Tricolor com três títulos e muitos gols - exatos 182 - o que o tornou o quinto maior goleador da história do São Paulo."
-  }, {
+    },
+    {
       name: "Müller",
       position: "Ponta-direita",
       avatar: "http://www.saopaulofc.net/media/35061/muller.jpg",
       description: "Muller foi um jogador marcante no São Paulo, com participação fundamental nos jogos que nos deram os dois primeiros títulos mundiais. Fez uma jogada excepcional contra o Barcelona, em 1992, e fez o gol da vitória contra o Milan em 93, de costas, calcanhar, sobrecoxa, barriga de perna ou algo que o valha - e que valeu muito. Foi importante também em várias outras conquistas, graças não só a uma diferenciada velocidade, mas, principalmente, a uma inteligência incrível dentro do campo. Disputou três Copas do Mundo (1986 a 1994), as duas primeiras como titular."
-  }, {
+    },
+    {
       name: "Amoroso",
       position: "Atacante",
       avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCKIGAX-WTVCkW_NxCnDrqisiGnXya8765vi6eKpiJAOn3aKoATMGqUqvgPc_JyzD50T0&usqp=CAU",
       description: "Chegou ao São Paulo em junho de 2005, após passagem vitoriosa pela Europa, sendo artilheiro do Campeonato Italiano pela Udinese, e do Campeonato Alemão pelo Borussia Dortmund, onde também conquistou o título nacional. E logo após a chegada já encarou um desafio duríssimo, que era a disputa da semifinal da Libertadores contra o River Plate-ARG, no jogo de volta em Buenos Aires, anotou o gol que selou a vitória do Tricolor por 3 a 2."
-  }, {
+    },
+    {
       name: "Telê Santana",
       position: "Técnico",
       avatar: "http://www.saopaulofc.net/media/2298/tele.jpg",
       description: "Este espaço é pouco para descrever tudo o que este grande técnico fez pelo clube. Telê teve duas passagens pelo Tricolor (1973 e 1990-1996), e é o técnico mais vencedor da história são-paulina. Ao todo foram dez títulos oficiais conquistados, incluindo os bicampeonatos da Taça Libertadores da América e do Mundial Interclubes, que elevaram o nome do São Paulo FC a um patamar nunca antes atingido. Eterno ídolo da torcida, que até hoje canta seu nome nos jogos do time, sua marca registrada era a disciplina imposta a seus comandados. Tudo em prol da perfeição técnica, alcançada mediante treinamento constante e rigidez de conduta."
-  }]
+    }
+  ]
 }
